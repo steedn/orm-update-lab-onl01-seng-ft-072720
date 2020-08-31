4ab9d58992ce0a35fe0c3c1ef5c9ccaafd0ffe9b
Nathan Steed <steedn@outlook.com>
2020-08-31 05:32:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,11 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade
+  attr_accessor :name, :grade, :id 
 
   def initialize(name, grade)
     @name = name
     @grade = grade
+    @id = nil
   end
 end
