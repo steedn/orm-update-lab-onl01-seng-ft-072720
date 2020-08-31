d31490cc15dbf04ef8fb019588d2a447be7a4880
Nathan Steed <steedn@outlook.com>
2020-08-31 05:59:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,6 +53,9 @@ class Student
     # binding.pry
   end
   def update
+    def update
+   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+   DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   def self.find_by_name(name)
     sql = <<-SQL
