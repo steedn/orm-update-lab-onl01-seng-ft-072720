e0aecda3bc08ea79c7a072f28c72227e0ce6efea
Nathan Steed <steedn@outlook.com>
2020-08-31 05:58:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,5 +55,15 @@ class Student
   def update
   end
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
