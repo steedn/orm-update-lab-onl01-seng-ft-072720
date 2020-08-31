fd9c8beb9665225affbf2f5ce38d290d223e5248
Nathan Steed <steedn@outlook.com>
2020-08-31 06:00:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,13 +48,12 @@ class Student
     # student.save
   end
 
-  def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
-    # binding.pry
-  end
+    def self.new_from_db(row)
+      id = row[0]
+      name = row[1]
+      grade = row[2]
+      self.new(id, name, grade)
+    end
   def update
    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
    DB[:conn].execute(sql, self.name, self.grade, self.id)
