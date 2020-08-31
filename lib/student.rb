c49a3db7ef514efe217003fe37dae4fe60a97f9e
Nathan Steed <steedn@outlook.com>
2020-08-31 05:48:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,11 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
-  def self.create
+  def self.create(name, grade)
+    sql = <<-SQL
+    INSERT INTO students (name, grade) VALUES
+    (?,?)
+    SQL
+    DB[:conn].execute(sql, name, grade)
   end
 end
